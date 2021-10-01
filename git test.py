3231af86897bea0c3b0fe1567c84d9acb6eb66ef
Commeilfaut <91380722+Commeilfaut@users.noreply.github.com>
2021-10-01 19:43:46 +0300

    next

--- git test.py
+++ git test.py
@@ -1,17 +1,16 @@
 import sys
-
 x = len(sys.argv)
 
-if x == 2:
+if x == 3:
     print("Аргументы выданы: " + str(sys.argv))
 else:
     print("Аргументы не выданы")
+print(sys.argv[1])
 
-with open(input("Ведите имя файла ")) as f:
-    with open("out.txt", "w") as f1:
-        with open("out2.txt", "w") as f2:    
-            for line in f:
-                if "adapter" in line:
-                    f1.write(line)
-                if "breakpoint" in line:
-                    f2.write(line)
+print(sys.argv[2])
+
+with open (sys.argv[1]) as f:
+    with open(sys.argv[2], "w") as f1:
+        for line in f:
+            if "adapter" in line:
+                f1.write(line)
